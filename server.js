f62a307584bb790d3891993ecd7d4d16c6cdff11
chandra8120 <chandrasekhar8120@gmail.com>
2024-10-03 11:00:43 +0530

    remove server js file

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ app.use(express.json());
 app.use("/", router);
 
 mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true
   })
   .then(() => console.log("mongodb connected successfully"))
   .catch((err) => console.log("mongodb connection error", err));
