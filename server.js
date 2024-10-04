823023f5001adba30799fb50979c57a3cfcc35e0
chandra8120 <chandrasekhar8120@gmail.com>
2024-10-04 15:02:24 +0530

    saved server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000; // Default port if process.env.PORT is un
 app.use(express.json());
 app.use("/", router);
 
-// Log the Mongo URI for debugging
+
 console.log("Mongo URI:", process.env.MONGO_URI);
 
 mongoose.connect(process.env.MONGO_URI)
